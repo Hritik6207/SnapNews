964ebf84fc57831dd0b6af022558122c904380b3
Hritik kumar Sinha <hs8985@srmist.edu.in>
2024-01-23 01:50:26 +0530

    SnapNews

--- src/Components/News.js
+++ src/Components/News.js
@@ -38,7 +38,7 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewsMonkey - Headlines</h2>
+        <h2>SnapNews - Headlines</h2>
 
         <div className="row">
           {this.state.articles.map((element)=>{
