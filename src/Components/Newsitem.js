bfad75eb97b49ea83289be71bbc293972ca12b4a
Hritik kumar Sinha <hs8985@srmist.edu.in>
2024-01-28 14:13:19 +0530

    Added badges for the source from where the news are coming to the news card.

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let {title,description,imgurl,newsurl}=this.props
+    let { title, description, imgurl, newsurl, source } = this.props;
     return (
-      <div className='my-3'>
-        
+      <div className="my-3">
         <div className="card">
-        <img src={!imgurl?"https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw":imgurl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}...</h5>
-    <p className="card-text">{description}...</p>
-    <a href={newsurl} target='blank' className="btn btn-sm btn-warning">Read full article</a>
-  </div>
-</div>
+          <div className="contain">
+            <span
+              className="badge rounded-pill bg-warning"
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                position: "absolute",
+                right: "0",
+              }}
+            >
+              {source}
+            </span>
+          </div>
+          <img
+            src={
+              !imgurl
+                ? "https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw"
+                : imgurl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+
+            <p className="card-text">{description}...</p>
+            <a href={newsurl} target="blank" className="btn btn-sm btn-warning">
+              Read full article
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
